ba3475a077fdaf0e432502369548ac2bde61b18f
Helium5t <10533138@polimi.it>
2020-11-15 16:55:44 +0100

    Fixed Objects through portal and added timed door event

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -12,18 +12,19 @@ public class DragObject : MonoBehaviour
 
     private RaycastHit hit;
     private float distanceFromMousePointer;
+    [SerializeField][Range(20f,500f)] private float maxPickUpDistance = 30f;
 
     private Rigidbody draggedObjectRb;
 
-    private void Update()
+    private void LateUpdate()
     {
         if (Input.GetMouseButton(0))
         {
             Ray screenPointToRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+            Debug.DrawRay(screenPointToRay.origin,screenPointToRay.direction * maxPickUpDistance,Color.red);
             if (!draggedObj)
             {
-                if (Physics.Raycast(screenPointToRay, out hit) && hit.rigidbody)
+                if (Physics.Raycast(screenPointToRay, out hit,maxPickUpDistance) && hit.rigidbody)
                 {
                     draggedObj = hit.transform;
                     distanceFromMousePointer = hit.distance;
@@ -31,13 +32,37 @@ public class DragObject : MonoBehaviour
                 }
             }
             else
-            {
+            {   
+                Vector3 targetPos  = Vector3.zero;
+                if(Physics.Raycast(screenPointToRay,out hit,maxPickUpDistance,LayerMask.GetMask("Portal"))){
+                    //MeshRenderer portalScreen = hitPortal.transform.Find("Screen").GetComponent<MeshRenderer>();
+                    Portal hitPortal = hit.transform.gameObject.GetComponentInParent<Portal>();
+                    if(hit.distance < distanceFromMousePointer){
+                        Vector3 portalRayOrigin = (hit.point - hitPortal.transform.position) + hitPortal.linkedPortal.transform.position;
+                        Vector3 portalRayDirection = screenPointToRay.direction;
+                        Debug.DrawRay(portalRayOrigin,portalRayDirection*50f,Color.green);
+                        Ray portalRay = new Ray(portalRayOrigin,portalRayDirection);
+                        if(Vector3.Distance(draggedObj.position,hitPortal.transform.position) > Vector3.Distance(draggedObj.position,hitPortal.linkedPortal.transform.position)){
+                            targetPos = portalRay.GetPoint(distanceFromMousePointer - hit.distance);
+                        }
+                        else{
+                            targetPos = hit.point + 0.2f*screenPointToRay.direction;
+                        }
+                    }
+                    else{
+                        targetPos = screenPointToRay.GetPoint(distanceFromMousePointer);
+                    }
+                }
+                else{
+                    targetPos = screenPointToRay.GetPoint(distanceFromMousePointer);
+                }
                 // Calculate needed speed
-                Vector3 vel = (screenPointToRay.GetPoint(distanceFromMousePointer) - draggedObj.position) * mouseFollowSpeed;
+                Vector3 vel = ( targetPos - draggedObj.position) * mouseFollowSpeed;
 
                 if (vel.magnitude > maxObjectSpeed) vel *= maxObjectSpeed / vel.magnitude;
                 draggedObjectRb.velocity = vel;
             }
+            
         }
         else
         {
---
+++ Assets/Scripts/TimerDoorTrigger.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerDoorTrigger : MonoBehaviour,TriggerInterface
+{
+    private Vector3 targetPos;
+    private float movementAmount = 10f;
+
+    [SerializeField] private float timeToWait = 10f;
+    
+    private void Start() {
+        targetPos = transform.position;   
+    }
+    private void Update() {
+        if(transform.position != targetPos){
+            transform.position = Vector3.Lerp(transform.position,targetPos,Time.deltaTime*2);
+        }
+    }
+
+    public void Trigger(){
+        StartCoroutine(OpenAndClose(timeToWait));
+    }
+
+    public IEnumerator OpenAndClose(float timeToWait){
+        Vector3 startingPos = transform.position;
+        targetPos = transform.position + Vector3.up * movementAmount;
+        yield return new WaitForSeconds(timeToWait);
+        targetPos = startingPos;
+    }
+
+}
