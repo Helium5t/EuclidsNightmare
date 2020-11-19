3614bbb6d3db9181243a83790033bb75d1c5e612
AndreDile996 <andre.dile96@gmail.com>
2020-11-19 21:39:26 +0100

    Crazy Rooms

---
+++ Assets/Scripts/corridor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class corridor : MonoBehaviour
+{
+    [SerializeField]
+    GameObject takeUp;
+    bool IsOpened = false;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (IsOpened == false)
+        {
+
+            takeUp.transform.position += new Vector3(0, -100, 0);
+
+            IsOpened = true;
+           
+
+        }
+        
+    }
+}
