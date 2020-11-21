30bc251d8dc813c7ea1f1bf0240788340ce30175
AndreDile996 <andre.dile96@gmail.com>
2020-11-21 11:30:29 +0100

    mirror

---
+++ Assets/Scripts/PortalScripts/PillarTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            gameObject.layer = 0;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.layer = 0;
+            }
+           
+        }
+
+    }
+}
