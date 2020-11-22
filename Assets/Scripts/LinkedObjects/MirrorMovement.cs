0dc9d16a7f8e9557d7eb55b93a4ae7823168e418
Helium5t <10533138@polimi.it>
2020-11-22 23:22:13 +0100

    Refactor Hierarchy, added mirror and pillars

--- Assets/Scripts/LinkedObjects/MirrorMovement.cs
+++ Assets/Scripts/LinkedObjects/MirrorMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mirrorMovement : MonoBehaviour
+public class MirrorMovement : MonoBehaviour
 {
 
     [SerializeField] private Transform toFollow;
---
+++ Assets/Scripts/MirrorCamera.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorCamera : MonoBehaviour
+{
+    [SerializeField] private Camera trackedCamera;
+    private Camera mirrorCamera;
+    [SerializeField] private Transform mirror;
+
+    
+    [SerializeField] private float nearClipOffset = 0.05f;
+    [SerializeField] private float nearClipLimit = 0.2f;
+
+    
+    // Start is called before the first frame update
+    private void Start() {
+        mirror = transform.parent;
+        mirrorCamera = GetComponent<Camera>();
+        Material mirrorMaterial = new Material(mirror.GetComponent<MeshRenderer>().material);
+        mirrorCamera.targetTexture =  new RenderTexture(Screen.width,Screen.height,24);
+        mirrorMaterial.mainTexture = mirrorCamera.targetTexture;
+        mirror.GetComponent<MeshRenderer>().material = mirrorMaterial;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPreRender(){
+        if (!CameraUtility.VisibleFromCamera (mirror.GetComponent<MeshRenderer>(), trackedCamera) ) {
+            return;
+        }
+        Vector3 objectPos = trackedCamera.transform.position;
+        objectPos = mirror.position  -objectPos ;
+        objectPos = mirror.position + ReflectV(ReflectV(objectPos,mirror.right),mirror.forward); 
+        transform.position = objectPos;
+        transform.forward = ReflectV(trackedCamera.transform.forward,mirror.up);
+        SetNearClipPlane();
+    }
+
+    void SetNearClipPlane () {
+        // Learning resource:
+        // http://www.terathon.com/lengyel/Lengyel-Oblique.pdf
+        Transform clipPlane = mirror.transform;
+        int dot = System.Math.Sign (Vector3.Dot (clipPlane.up, mirror.position - transform.position));
+
+        Vector3 camSpacePos = mirrorCamera.worldToCameraMatrix.MultiplyPoint (clipPlane.position);
+        Vector3 camSpaceNormal = mirrorCamera.worldToCameraMatrix.MultiplyVector (clipPlane.up) * dot;
+        float camSpaceDst = -Vector3.Dot (camSpacePos, camSpaceNormal) + nearClipOffset;
+
+        // Don't use oblique clip plane if very close to portal as it seems this can cause some visual artifacts
+        if (Mathf.Abs (camSpaceDst) > nearClipLimit) {
+            Vector4 clipPlaneCameraSpace = new Vector4 (camSpaceNormal.x, camSpaceNormal.y, camSpaceNormal.z, camSpaceDst);
+
+            // Update projection based on new clip plane
+            // Calculate matrix with player cam so that player camera settings (fov, etc) are used
+            mirrorCamera.projectionMatrix = mirrorCamera.CalculateObliqueMatrix (clipPlaneCameraSpace);
+        } else {
+            mirrorCamera.projectionMatrix = mirrorCamera.projectionMatrix;
+        }
+    }
+
+
+    Vector3 getReflectedPos(){
+        return Vector3.zero;
+    }
+
+    float getSide(Vector3 trackedObject){
+        return Mathf.Sign(Vector3.Dot(trackedObject,transform.forward));
+    }
+
+    
+    Vector3 ReflectV(Vector3 inDirection, Vector3 inNormal)
+        {
+            float nx = inNormal.x;
+            float ny = inNormal.y;
+            float nz = inNormal.z;
+            float dx = inDirection.x;
+            float dy = inDirection.y;
+            float dz = inDirection.z;
+            float factor = -2F * Vector3.Dot(inNormal, inDirection);
+            return new Vector3(factor *nx + dx,
+                factor * ny + dy,
+                factor * nz + dz);
+        }
+
+
+}
