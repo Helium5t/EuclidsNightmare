8282bcad474fed2f9f17a8a9c9376f23cab3870f
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-11-26 21:55:06 +0100

    Added PauseMenu + UI

---
+++ Assets/Scripts/ButtonClickerManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClickerManager : MonoBehaviour
+{
+    [SerializeField] private Button[] _buttons;
+
+    private void OnEnable()
+    {
+        foreach (Button button in _buttons) button.onClick.AddListener(() => ButtonCallback(button));
+    }
+    
+    private void ButtonCallback(Button pressedButton)
+    {
+        if (pressedButton == _buttons[0])
+        {
+            Debug.Log("Clicked " + _buttons[0].name);
+            GameManager.Instance.StartGame();
+        }
+
+        if (pressedButton == _buttons[1])
+        {
+            Debug.Log("Clicked " + _buttons[1].name);
+            GameManager.Instance.QuitGame();
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (Button button in _buttons) button.onClick.RemoveAllListeners();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utility;
+
+public class GameManager : Singleton<GameManager>
+{
+    public static bool GameIsPaused;
+    [SerializeField] private GameObject pauseMenuUI;
+
+    /*private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused) ResumeGame();
+            else PauseGame();
+        }
+    }*/
+
+    public void QuitGame() => Application.Quit();
+
+    public void StartGame()
+    {
+        /*
+         * TODO: this is a placeHolder, later I'll call smth like LevelManager.startLevel(FirstLevel)
+         */
+        SceneManager.LoadScene("GrowingApart");
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        GameIsPaused = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
