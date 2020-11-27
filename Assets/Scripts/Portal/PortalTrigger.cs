bdfbf1b25b9bca4b823f3798ae1d1acb72edfced
Helium5t <10533138@polimi.it>
2020-11-27 18:44:53 +0100

    Updated up is Down

--- Assets/Scripts/Portal/PortalTrigger.cs
+++ Assets/Scripts/Portal/PortalTrigger.cs
@@ -6,8 +6,10 @@ public class PortalTrigger : MonoBehaviour
 {
     [SerializeField] GameObject player;
     [SerializeField] Portal []portal;
+
     void OnTriggerEnter(Collider col)
     {
+        Debug.Log(transform.position);
         if (col.tag == "Player")
         {
             for (int i=0; i<2; i++)
@@ -21,9 +23,9 @@ public class PortalTrigger : MonoBehaviour
     {
         Transform tran;
         if (col.tag == "Player")
-        {
+        {       
             tran = player.transform;
-            if(Vector3.Distance(tran.position,portal[0].transform.position)< Vector3.Distance(tran.position, portal[1].transform.position))
+            if(tran.position.x - transform.position.x < 0.0000f)
             {
                 portal[0].linkedPortal.gameObject.SetActive(true);
                 portal[1].linkedPortal.gameObject.SetActive(false);
