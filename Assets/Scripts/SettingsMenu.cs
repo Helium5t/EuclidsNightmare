c260376e556214cdbc06b415888bc3980f00d0a3
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-12-14 22:31:31 +0100

    Added Settings Menu UI v2

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,35 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
+using FMOD.Studio;
+using Utility;
 
 public class SettingsMenu : MonoBehaviour
 {
     public Dropdown resolutions;
-    public AudioMixer audioMixer;
+    public Dropdown graphicsQualities;
 
     private Resolution[] possibleResolutions;
 
+    private EventInstance SFXVolumeTestEvent;
+    private Bus masterBus;
+    private Bus musicBus;
+    private Bus SFXbus;
+
+    // Set these values the same as the UI Sliders!
+    private float MasterVolume = 1f;
+    private float MusicVolume = 0.5f;
+    private float SFXVolume = 0.5f;
+
+    private void Awake()
+    {
+        masterBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MasterBus);
+        musicBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MusicBus);
+        SFXbus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.SFXBus);
+        SFXVolumeTestEvent = FMODUnity.RuntimeManager.CreateInstance(GameSoundPaths.SfxVolumeTestPath);
+    }
+
+    private void Update()
+    {
+        masterBus.setVolume(MasterVolume);
+        musicBus.setVolume(MusicVolume);
+        SFXbus.setVolume(SFXVolume);
+    }
+
     private void Start()
     {
-        possibleResolutions = Screen.resolutions;
+        graphicsQualities.GetComponent<Dropdown>().value = QualitySettings.GetQualityLevel();
 
-        List<string> resoStringList = new List<string>();
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < possibleResolutions.Length; i++)
-        {
-            resoStringList.Add(possibleResolutions[i].width + " x " + possibleResolutions[i].width);
+        UpdateResolutionsDropdown();
+    }
 
-            if (possibleResolutions[i].width == Screen.currentResolution.width &&
-                possibleResolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
+    public void setDisableAudio(bool isAudioDisabled) => throw new NotImplementedException("SetDisable audio is not implemented, yet :=)");
 
-        resolutions.AddOptions(resoStringList);
-        resolutions.value = currentResolutionIndex;
-        resolutions.RefreshShownValue();
+    public void setMasterVolume(float newMasterVolume) => MasterVolume = newMasterVolume;
+
+    public void setMusicBusVolume(float newMusicBusVolume) => MusicVolume = newMusicBusVolume;
+
+    public void SetSfxVolume(float newSfxVolume)
+    {
+        SFXVolume = newSfxVolume;
+
+        SFXVolumeTestEvent.getPlaybackState(out PLAYBACK_STATE playbackState);
+        if (playbackState != PLAYBACK_STATE.PLAYING) SFXVolumeTestEvent.start();
     }
 
     public void setResolution(int resolutionIndex)
@@ -44,4 +70,27 @@ public class SettingsMenu : MonoBehaviour
 
     public void changeGraphicsQuality(int graphicsQualityIndex) =>
         QualitySettings.SetQualityLevel(graphicsQualityIndex);
+
+    private void UpdateResolutionsDropdown()
+    {
+        possibleResolutions = Screen.resolutions;
+
+        List<string> resolutionsStringList = new List<string>();
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < possibleResolutions.Length; i++)
+        {
+            resolutionsStringList.Add(possibleResolutions[i].width + " x " + possibleResolutions[i].width + " @" +
+                                      possibleResolutions[i].refreshRate);
+
+            if (possibleResolutions[i].width == Screen.currentResolution.width &&
+                possibleResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutions.AddOptions(resolutionsStringList);
+        resolutions.value = currentResolutionIndex;
+        resolutions.RefreshShownValue();
+    }
 }
