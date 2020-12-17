5cecad092dfe2564718318dc5c1f38b3df823a6c
itsNikk <nicolo.buganza@studenti.unimi.it>
2020-12-17 12:05:04 +0100

    UI Refactoring

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -9,31 +9,31 @@ public class SettingsMenu : MonoBehaviour
     public Dropdown resolutions;
     public Dropdown graphicsQualities;
 
-    private Resolution[] possibleResolutions;
+    private Resolution[] _possibleResolutions;
 
-    private EventInstance SFXVolumeTestEvent;
-    private Bus masterBus;
-    private Bus musicBus;
-    private Bus SFXbus;
+    private EventInstance _sfxVolumeTestEvent;
+    private Bus _masterBus;
+    private Bus _musicBus;
+    private Bus _sfxBus;
 
     // Set these values the same as the UI Sliders!
-    private float MasterVolume = 1f;
-    private float MusicVolume = 0.5f;
-    private float SFXVolume = 0.5f;
+    private float _masterVolume = 1f;
+    private float _musicVolume = 0.5f;
+    private float _sfxVolume = 0.5f;
 
     private void Awake()
     {
-        masterBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MasterBus);
-        musicBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MusicBus);
-        SFXbus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.SFXBus);
-        SFXVolumeTestEvent = FMODUnity.RuntimeManager.CreateInstance(GameSoundPaths.SfxVolumeTestPath);
+        _masterBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MasterBus);
+        _musicBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.MusicBus);
+        _sfxBus = FMODUnity.RuntimeManager.GetBus(GameBussesPaths.SFXBus);
+        _sfxVolumeTestEvent = FMODUnity.RuntimeManager.CreateInstance(GameSoundPaths.SfxVolumeTestPath);
     }
 
     private void Update()
     {
-        masterBus.setVolume(MasterVolume);
-        musicBus.setVolume(MusicVolume);
-        SFXbus.setVolume(SFXVolume);
+        _masterBus.setVolume(_masterVolume);
+        _musicBus.setVolume(_musicVolume);
+        _sfxBus.setVolume(_sfxVolume);
     }
 
     private void Start()
@@ -44,46 +44,46 @@ public class SettingsMenu : MonoBehaviour
 
     public void ChangeFovValue(float fovValue) => Camera.main.fieldOfView = fovValue;
 
-    public void setDisableAudio(bool isAudioDisabled) => masterBus.setMute(isAudioDisabled);
+    public void SetDisableAudio(bool isAudioDisabled) => _masterBus.setMute(isAudioDisabled);
 
-    public void setMasterVolume(float newMasterVolume) => MasterVolume = newMasterVolume;
+    public void SetMasterVolume(float newMasterVolume) => _masterVolume = newMasterVolume;
 
-    public void setMusicBusVolume(float newMusicBusVolume) => MusicVolume = newMusicBusVolume;
+    public void SetMusicBusVolume(float newMusicBusVolume) => _musicVolume = newMusicBusVolume;
 
-    public void SetSfxVolume(float newSfxVolume)
+    public void SetSfxBusVolume(float newSfxVolume)
     {
-        SFXVolume = newSfxVolume;
+        _sfxVolume = newSfxVolume;
 
-        SFXVolumeTestEvent.getPlaybackState(out PLAYBACK_STATE playbackState);
-        if (playbackState != PLAYBACK_STATE.PLAYING) SFXVolumeTestEvent.start();
+        _sfxVolumeTestEvent.getPlaybackState(out PLAYBACK_STATE playbackState);
+        if (playbackState != PLAYBACK_STATE.PLAYING) _sfxVolumeTestEvent.start();
     }
 
-    public void setResolution(int resolutionIndex)
+    public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = possibleResolutions[resolutionIndex];
+        Resolution resolution = _possibleResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
-    public void setVsync(bool isVsyncActive) => QualitySettings.vSyncCount = isVsyncActive ? 1 : 0;
+    public void SetVsync(bool isVsyncActive) => QualitySettings.vSyncCount = isVsyncActive ? 1 : 0;
 
-    public void setFullScreen(bool isFullScreen) => Screen.fullScreen = isFullScreen;
+    public void SetFullScreen(bool isFullScreen) => Screen.fullScreen = isFullScreen;
 
-    public void changeGraphicsQuality(int graphicsQualityIndex) =>
+    public void ChangeGraphicsQuality(int graphicsQualityIndex) =>
         QualitySettings.SetQualityLevel(graphicsQualityIndex);
 
     private void UpdateResolutionsDropdown()
     {
-        possibleResolutions = Screen.resolutions;
+        _possibleResolutions = Screen.resolutions;
 
         List<string> resolutionsStringList = new List<string>();
         int currentResolutionIndex = 0;
-        for (int i = 0; i < possibleResolutions.Length; i++)
+        for (int i = 0; i < _possibleResolutions.Length; i++)
         {
-            resolutionsStringList.Add(possibleResolutions[i].width + " x " + possibleResolutions[i].width + " @" +
-                                      possibleResolutions[i].refreshRate);
+            resolutionsStringList.Add(_possibleResolutions[i].width + " x " + _possibleResolutions[i].width + " @" +
+                                      _possibleResolutions[i].refreshRate);
 
-            if (possibleResolutions[i].width == Screen.currentResolution.width &&
-                possibleResolutions[i].height == Screen.currentResolution.height)
+            if (_possibleResolutions[i].width == Screen.currentResolution.width &&
+                _possibleResolutions[i].height == Screen.currentResolution.height)
             {
                 currentResolutionIndex = i;
             }
