3f66e447c93becffdbfa76c9c5cbf39631c809f0
itsNikk <nicolo.buganza@studenti.unimi.it>
2020-12-22 12:58:35 +0100

    New Euclid Sounds added + refactoring
    Added Breathing Sound

--- Assets/Scripts/FMODStudioFirstPersonFootsteps.cs
+++ Assets/Scripts/FMODStudioFirstPersonFootsteps.cs
@@ -1,12 +1,9 @@
-﻿using FMODUnity;
+﻿using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 using Utility;
 using Random = UnityEngine.Random;
 
-/*
- * Attach this script to the Game Object you're using as your First Person Controller.
- * Only include one instance of this script as a component in each scene of the game.
- */
 public class FMODStudioFirstPersonFootsteps : MonoBehaviour
 {
     //These variables will all be set in the Inspector tab of Unity's Editor by either us, or the 'FMODStudioFootstepsEditor' script.
@@ -14,21 +11,47 @@ public class FMODStudioFirstPersonFootsteps : MonoBehaviour
 
     [Header("FMOD Settings")]
     //TODO: we have GameSoundsPaths static class so we could remove the following two vars... :)
-    [SerializeField] [EventRef] private string FootstepsEventPath; // Use this in the Editor to select our Footsteps Event.
-    [SerializeField] [EventRef] private string JumpingEventPath; // Use this in the Editor to select our Jumping Event.
-    [SerializeField] private string MaterialParameterName;  // Use this in the Editor to write the name of the parameter that controls which material the player is currently walking on.
-    [SerializeField] private string SpeedParameterName; // Use this in the Editor to write the name of the parameter that controls which footstep speed needs to be heard.
-    [SerializeField] private string JumpOrLandParameterName; // Use this in the Editor to write the name of the parameter that controls whether or not a jumping or a landing sound needs to be heard.
+    [SerializeField] [EventRef] private string FootstepsEventPath; 
+    [SerializeField] [EventRef] private string JumpingEventPath;
+    /* Use this in the Editor to write the name of the parameter that controls
+     which material the player is currently walking on.*/
+    [SerializeField] private string MaterialParameterName;
+    /*
+     * Use this in the Editor to write the name of the parameter that controls which footstep speed needs to be heard.
+     */
+    [SerializeField] private string SpeedParameterName; 
+    /*
+     * Use this in the Editor to write the name of the parameter that controls
+     * whether or not a jumping or a landing sound needs to be heard.
+     */
+    [SerializeField] private string JumpOrLandParameterName; 
     
     [Header("Playback Settings")]
-    [SerializeField] private float StepDistance = 1.5f; // Select how far the player must travel before they hear a footstep. This will then remain a constant and will not change.
-    [SerializeField] private float RayDistance = 1.2f; // Select how far the raycast will travel down to when checking for a floor. This will then remain a constant and will not change.
-    [SerializeField] private float StartRunningTime = 0.3f; // Set a time. If the time between each step the player takes is less than this value, the player will start to hear running footsteps. This will then remain a constant and will not change.
-    [SerializeField] private string JumpInputName; // In Unity, go Edit -> Project Settings -> Input Manager. Then find the name of the input that controls which key/button the player must press in order to jump (it's proably called "Jump"). Then once you know it's name, write it into this variable in the Inspector tab. This will then remain a constant and will not change.
+    // Select how far the player must travel before they hear a footstep.
+    [SerializeField] private float StepDistance = 1.5f;
+    // Select how far the raycast will travel down to when checking for a floor.
+    [SerializeField] private float RayDistance = 1.2f; 
+    
+    /* Set a time.
+     * If the time between each step the player takes is less than this value,
+     * the player will start to hear running footsteps.
+     */
+    [SerializeField] private float StartRunningTime = 0.3f;
+    /* In Unity, go Edit -> Project Settings -> Input Manager.
+     * Then find the name of the input that controls which key/button the player must press in order to jump
+     * (it's probably called "Jump").
+     * Then once you know it's name, write it into this variable in the Inspector tab. */
+    [SerializeField] private string JumpInputName; 
     
     [Space]
     [Header("Walking material settings")]
-    public string[] MaterialTypes; // This is an array of strings. In the inspector we can decide how many Material types we have in FMOD by setting the size of this array. Depending on the size, the array will then create a certain amount of strings for us to fill in with the name of each of our footstep materials for our scripts to use. This will then remain a constant and will not change.
+    /*
+     * In the inspector we can decide how many Material types we have in FMOD by setting the size of this array.
+     * Depending on the size,
+     * the array will then create a certain amount of strings for us to fill in with the name of each of our footstep
+     * materials for our scripts to use.
+     */
+    public string[] MaterialTypes; 
     #endregion
     
     // This will be told by the 'FMODStudioFootstepsEditor' script which Material has been set as the default.
@@ -39,16 +62,16 @@ public class FMODStudioFirstPersonFootsteps : MonoBehaviour
     #region PrivateVariables
 
     //These variables are used to control when the player executes a footstep.
-    private float StepRandom; // This will be set as random number, which will later be added to the StepDistance to add a little variaiton to the length in steps.
-    private Vector3 PrevPos; // This will old the co-ordinates of the previous postion the player was in during the last frame.
+    private float StepRandom; // This will be set as random number, which will later be added to the StepDistance to add a little variation to the length in steps.
+    private Vector3 PrevPos; // This will hold the co-ordinates of the previous position the player was in during the last frame.
     private float DistanceTravelled; // This will hold a value that how represent how far the player has travelled since they last took a step.
     
     //These variables are used when checking the Material type the player is on top of.
-    private RaycastHit hit; // Will holds information about the GameObject that the raycast hits.
+    private RaycastHit hit; 
     private int FmodMaterialValue; // We'll use this to set the value of our FMOD Material Parameter.
     
     //These booleans will hold values that tell us if the player is touching the ground currently and if they were touching it during the last frame.
-    private bool isPlayerTouchingGround; // This will hold a value to represent whether or not the player is touching the ground. True = Grounded, False = Not Grounded.
+    private bool isPlayerTouchingGround; // This will hold a value to represent whether or not the player is touching the ground.
     private bool isPreviouslyTouchingGround; // This will hold a value to represent whether or not the player was touching the ground during the last frame. True = Was Grounded, False = Wasn't Grounded.
     
     //These floats help us determine when the player should be running.
@@ -56,6 +79,7 @@ public class FMODStudioFirstPersonFootsteps : MonoBehaviour
     private int FmodPlayerRunning; // We'll use to set the value of our FMOD Switch Speed Parameter.
 
     private Transform _transform; //Transform component caching
+    private EventInstance breathingEventInstance;
 
     #endregion
     
@@ -65,7 +89,10 @@ public class FMODStudioFirstPersonFootsteps : MonoBehaviour
     {
         StepRandom = Random.Range(0f, 0.3f);
         // 'PrevPos' now holds the location that the player is starting at as co-ordinates (x, y, z).
-        PrevPos = _transform.position;      
+        PrevPos = _transform.position;
+        breathingEventInstance = FMODUnity.RuntimeManager.CreateInstance(GameSoundPaths.BreathingEventPath);
+        RuntimeManager.AttachInstanceToGameObject(breathingEventInstance,transform,GetComponent<Rigidbody>());
+        breathingEventInstance.start();
     }
 
 
@@ -113,11 +140,11 @@ public class FMODStudioFirstPersonFootsteps : MonoBehaviour
         #endregion
     }
 
-/// <summary>
-/// This method when performed will find out what material our player is currently on top of
-/// and will update the value of 'F_MaterialValue' accordingly, to represent that value.
-/// </summary>
-private void MaterialCheck() 
+    /// <summary>
+    /// This method when performed will find out what material our player is currently on top of
+    /// and will update the value of 'F_MaterialValue' accordingly, to represent that value.
+    /// </summary>
+    private void MaterialCheck() 
     {
         
         if (Physics.Raycast(_transform.position, Vector3.down, out hit, RayDistance)) 
@@ -139,10 +166,10 @@ private void MaterialCheck()
             FmodMaterialValue = DefaultMaterialValue; // Then again, we set 'FmodMaterialValue' to match the value of 'DefaultMaterialValue'.
     }
 
-/// <summary>
-/// This method when performed will update the 'F_PlayerRunning' variable, to find out if the player should be hearing heavy running footsteps.
-/// </summary>
-private void SpeedCheck() 
+    /// <summary>
+    /// This method when performed will update the 'F_PlayerRunning' variable, to find out if the player should be hearing heavy running footsteps.
+    /// </summary>
+    private void SpeedCheck() 
     {
         // Can be substituted with -> FmodPlayerRunning = TimeTakenSinceStep < StartRunningTime ? 1 : 0;
         if (TimeTakenSinceStep < StartRunningTime) // Remember that 'TimeTakenToStep' is a running timer. This method will only be performed as the player takes a step. So if this time is less than the value set to the 'StartRunningTime' variable, we can asume the player is moving fast enough to warrent a heavy running footstep to be played. So therefore...
@@ -152,19 +179,19 @@ private void SpeedCheck()
         TimeTakenSinceStep = 0f; // Finally, now that the player has taken the correct step, we reset our timer 'TimeTakenToStep' back to 0. Now th timer is tracking how much time has passed since the player took that last step.
     }
 
-/// <summary>
-/// This method will update the 'PlayerTouchingGround' variable, to find out if the player is currently touching the ground or not.
-/// </summary>
-private void GroundedCheck()
-{
-    Physics.Raycast(_transform.position, Vector3.down, out hit, RayDistance); // First, a raycast is fired from the location of the player, downwards towards the ground. It travels for as far as we set the 'RayDistance' variable to in UNity's Inspector and then stores information about whatever it comes into contact with inside the 'hit' variable. 
-    isPlayerTouchingGround = hit.collider;
-}
+    /// <summary>
+    /// This method will update the 'PlayerTouchingGround' variable, to find out if the player is currently touching the ground or not.
+    /// </summary>
+    private void GroundedCheck()
+    {
+        Physics.Raycast(_transform.position, Vector3.down, out hit, RayDistance); // First, a raycast is fired from the location of the player, downwards towards the ground. It travels for as far as we set the 'RayDistance' variable to in UNity's Inspector and then stores information about whatever it comes into contact with inside the 'hit' variable. 
+        isPlayerTouchingGround = hit.collider;
+    }
 
-/// <summary>
-/// When this method is performed, our footsteps event in FMOD will be told to play.
-/// </summary>
-private void PlayFootstep() 
+    /// <summary>
+    /// When this method is performed, our footsteps event in FMOD will be told to play.
+    /// </summary>
+    private void PlayFootstep() 
     {
         if (isPlayerTouchingGround) // First we check to see the player is touching the ground.
         {
@@ -177,14 +204,14 @@ private void PlayFootstep()
         }
     }
 
-/// <summary>
-/// // When this method is performed our Jumping And Landing event in FMOD will be told to play.
-/// A boolean variable is also created inside it's parameter brackets to be used inside this method.
-/// This variable will hold whichever value we gave this method when we called it in the Update function.
-/// </summary>
-/// <param name="fmodJumpOrLand"></param>
-private void PlayJumpOrLand(bool fmodJumpOrLand)     
-{
+    /// <summary>
+    /// // When this method is performed our Jumping And Landing event in FMOD will be told to play.
+    /// A boolean variable is also created inside it's parameter brackets to be used inside this method.
+    /// This variable will hold whichever value we gave this method when we called it in the Update function.
+    /// </summary>
+    /// <param name="fmodJumpOrLand"></param>
+    private void PlayJumpOrLand(bool fmodJumpOrLand)     
+    {
         FMOD.Studio.EventInstance jumpLandEvent = FMODUnity.RuntimeManager.CreateInstance(GameSoundPaths.PlayerJumpLandSoundPath); // First we create an FMOD event instance. We use the event path inside the 'JumpingEventPath' variable to find the event we want to play.
         FMODUnity.RuntimeManager.AttachInstanceToGameObject(jumpLandEvent, _transform, GetComponent<Rigidbody>()); // Next that event instance is told to play at the location that our player is currently at.
         jumpLandEvent.setParameterByName(MaterialParameterName, FmodMaterialValue); // Before the event is played, we set the Material Parameter to match the value of the 'F_MaterialValue' variable.
@@ -194,3 +221,7 @@ private void PlayJumpOrLand(bool fmodJumpOrLand)
     }
 }
 
+/*
+ * Attach this script to the Game Object you're using as your First Person Controller.
+ * Only include one instance of this script as a component in each scene of the game.
+ */
