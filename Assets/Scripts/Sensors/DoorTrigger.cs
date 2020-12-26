53cd36401b299110c548b28134869445e3ffb587
Sephirothbahamut <sephirothbahamut@live.it>
2020-12-26 16:49:35 +0100

    added respawn script with fade-out

--- Assets/Scripts/Sensors/DoorTrigger.cs
+++ Assets/Scripts/Sensors/DoorTrigger.cs
@@ -1,25 +1,26 @@
 ﻿using UnityEngine;
 
 namespace Sensors
-{
+    {
     [RequireComponent(typeof(AudioSource))]
     public class DoorTrigger : Target
-    {
+        {
+        [Header("Door")]
         private Animator animator = null;
         private static readonly int open = Animator.StringToHash("open");
 
         [SerializeField] public bool startOpen = false;
 
         private void Start()
-        {
+            {
             if (!animator && !TryGetComponent<Animator>(out animator)) animator = GetComponentInChildren<Animator>();
             SetAnimationBool(startOpen);
-        }
+            }
 
         protected override void activate() => SetAnimationBool(true);
 
         protected override void deactivate() => SetAnimationBool(false);
 
         private void SetAnimationBool(bool value) => animator.SetBool(open, value);
+        }
     }
-}
