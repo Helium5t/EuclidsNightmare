711441dff88d84ad8a08847d559a8b26ae4f78a4
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2020-12-31 17:49:32 +0100

    Create RotateAroundObject.cs
    Just a Utility Script for the Camera in the Otto Test Scene

---
+++ Assets/Scripts/Utility/RotateAroundObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    /// <summary>
+    /// This script is used only in the Otto Test Scene.
+    /// Just for fun!
+    /// </summary>
+    public class RotateAroundObject : MonoBehaviour
+    {
+        [SerializeField] private GameObject targetGameObject;
+        public int rotationDegreesPerSecond = 20;
+
+        private void Update() => transform.RotateAround(targetGameObject.transform.position, Vector3.up,
+            rotationDegreesPerSecond * Time.deltaTime);
+    }
+}
