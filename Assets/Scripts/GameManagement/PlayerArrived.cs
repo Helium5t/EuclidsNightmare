7654986e83cccd789fe91b14ef7099bffefa3978
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2021-01-04 22:51:16 +0100

    Create PlayerArrived.cs

---
+++ Assets/Scripts/GameManagement/PlayerArrived.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+namespace GameManagement
+{
+    /// <summary>
+    /// This simple script is only used in the ForcedPerspectiveIntro Scene. :=)
+    /// </summary>
+    public class PlayerArrived : MonoBehaviour
+    {
+
+        [SerializeField] private GameObject door;
+        private static readonly int doorAnimatorBool = Animator.StringToHash("open");
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player")) door.GetComponent<Animator>().SetBool(doorAnimatorBool, true);
+        }
+    }
+}
