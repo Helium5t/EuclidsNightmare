c5a19d2ee60ad51f8115c615d90458ce63fb68c3
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2021-01-04 22:29:02 +0100

    Update DoorSoundsManager.cs

--- Assets/Scripts/Utility/DoorSoundsManager.cs
+++ Assets/Scripts/Utility/DoorSoundsManager.cs
@@ -7,12 +7,6 @@ namespace Utility
         [SerializeField] private Animator doorAnimator;
         private static readonly int OpenCloseDoorBool = Animator.StringToHash("open");
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.O)) doorAnimator.SetBool(OpenCloseDoorBool, true);
-            if (Input.GetKeyDown(KeyCode.C)) doorAnimator.SetBool(OpenCloseDoorBool, false);
-        }
-
         public void PlayOpenDoorSound() =>
             FMODUnity.RuntimeManager.PlayOneShot("event:/Sounds/Door/OpenDoor", transform.position);
 
