e1e28a08229a4c836b504d9cafb421f62e008f02
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2021-01-05 15:20:06 +0100

    Update DoorTrigger.cs
    Some easy refactoring, nothing structural

--- Assets/Scripts/Sensors/Door/DoorTrigger.cs
+++ Assets/Scripts/Sensors/Door/DoorTrigger.cs
@@ -1,26 +1,25 @@
 ﻿using UnityEngine;
 
-namespace Sensors
-    {
+namespace Sensors.Door
+{
     [RequireComponent(typeof(AudioSource))]
     public class DoorTrigger : Target
-        {
-        [Header("Door")]
-        private Animator animator = null;
+    {
+        [Header("Door")] private Animator animator = null;
         private static readonly int open = Animator.StringToHash("open");
 
         [SerializeField] public bool startOpen = false;
 
         private void Start()
-            {
+        {
             if (!animator && !TryGetComponent<Animator>(out animator)) animator = GetComponentInChildren<Animator>();
             SetAnimationBool(startOpen);
-            }
+        }
 
         protected override void activate() => SetAnimationBool(true);
 
         protected override void deactivate() => SetAnimationBool(false);
 
         private void SetAnimationBool(bool value) => animator.SetBool(open, value);
-        }
     }
+}
