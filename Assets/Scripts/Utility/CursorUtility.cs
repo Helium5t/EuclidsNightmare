2758a700168479093e9c626929ab8f2cfd4a2d7e
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2021-01-08 15:48:38 +0100

    Create CursorUtility.cs

---
+++ Assets/Scripts/Utility/CursorUtility.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    public class CursorUtility : MonoBehaviour
+    {
+        public static void LockCursor()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        public static void UnlockCursor()
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
