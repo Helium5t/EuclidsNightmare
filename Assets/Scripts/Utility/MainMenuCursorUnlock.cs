43f751279a72597a36f8cf6f5116738997c34388
Nicolò Buganza <56979355+itsNikk@users.noreply.github.com>
2021-01-09 18:33:37 +0100

    Create MainMenuCursorUnlock.cs
    This class is used only in the MainMenu scene to make sure that the cursor is unlocked whenever it gets loaded.

---
+++ Assets/Scripts/Utility/MainMenuCursorUnlock.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Utility
+{
+    /// <summary>
+    /// This class is used only in the MainMenu scene to make sure that the cursor is unlocked whenever it gets loaded.
+    /// </summary>
+    public class MainMenuCursorUnlock : MonoBehaviour
+    {
+        private void Start() => CursorUtility.UnlockCursor();
+    }
+}
