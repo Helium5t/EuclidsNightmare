b4bd256750c89ee8576143f4c1ab733c6cc6e916
Helium5t <10533138@polimi.it>
2021-02-23 18:29:28 +0100

    Final Dump Commit. End of Project

---
+++ Assets/Scripts/Utility/EmissionChangeReceiver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmissionChangeReceiver : MonoBehaviour
+{
+    Material emissionMaterial;
+    // Start is called before the first frame update
+    private void Awake() {
+        foreach(Material m in GetComponent<MeshRenderer>().materials){
+            if(m.name.Contains("Light")){
+                emissionMaterial = m;
+            }
+        }
+    }
+    public void setEmissionColor(Color newColor){
+        emissionMaterial.SetColor("_EmissionColor",newColor);
+    }
+}
---
+++ Assets/Scripts/Utility/FollowPos.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPos : MonoBehaviour
+{
+    [SerializeField] private Transform followed;
+
+    private void Update() {
+        transform.position = followed.position;
+    }
+}
